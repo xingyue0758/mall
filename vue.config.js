bc7066525ca251bbfa3d17a5c359b373799af2f6
Virgil <1269663329@qq.com>
2022-08-16 21:00:05 +0800

    mall 1.0.0

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   transpileDependencies: true,
-  plugins: {
-    autoperfixer: {},
-    'postcss-px-to-viewport': {
-      viewportWidth: 375,
-      viewportHeight: 667,
-      unitPrecision: 5,
-      viewportUnit: 'vw',
-      SelectorBlackList: [],
-      minPixelValue: 1,
-      mediaQuery: false
-    }
-  }
+  
 
 }
